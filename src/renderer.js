c5d12d5e05d4ba7d2602dc26adc5ee606684598c
77f315 <77f315@gmail.com>
2024-09-27 23:11:32 +0300

    formatting

--- src/renderer.js
+++ src/renderer.js
@@ -1,10 +1,17 @@
 let timerinterval;
 let elapsed = 0;
+
+function formatTime(seconds){
+    const hours = String(Math.floor(seconds / 3600)).padStart(2, '0');
+    const minutes = String(Math.floor((seconds % 3600) /60)).padStart(2, '0');
+    const secs = String(seconds % 60).padStart(2, '0'); 
+    return `${hours}:${minutes}:${secs}`
+}
 function startTimer() {
     timerinterval = setInterval(() => {
         elapsed++;
-        document.getElementById('timer').innerText = elapsed
-    }, 1)
+        document.getElementById('timer').innerText = formatTime(elapsed)
+    }, 1000)
 }
 function stopTimer() {
     clearInterval(timerinterval);
